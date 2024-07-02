dacc3b4c9481baaf7007efd087911f895e8a4aa1
Nazar-debug <ipz23-n.khomenko@nubip.edu.ua>
2024-07-02 18:29:20 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double t1, t2, t3;
+    printf("Введіть три значення:\n");
+    scanf("%lf %lf %lf", &t1, &t2, &t3);
+
+    double total_rate = (1.0 / t1) + (1.0 / t2) + (1.0 / t3);
+    double time_needed = 1.0 / total_rate;
+
+    printf("Час, необхідний для з'їдання пирога: %.2f годин\n", time_needed);
+
+    return 0;
+}
