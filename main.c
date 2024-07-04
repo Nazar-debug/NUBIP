6a300e4031b6161bdaaea2721a71886af2bd8699
Nazar-debug <ipz23-n.khomenko@nubip.edu.ua>
2024-07-04 13:27:19 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,33 +1,36 @@
 #include <stdio.h>
-#include <windows.h>
 
-int equal_divisors(int n) {
-    int count = 0;
-    for (int r = 1; r < n; r++) {
-        if (n % r == 0) {
-            int m = (n / r) - 1;
-            if (m > 0 && n % m == 0) {
-                count++;
-            }
-        }
+
+int countSequences(int n) {
+    if (n == 1) return 2;
+    if (n == 2) return 4;
+
+    int mod = 12345;
+    int a = 2, b = 4, c = 7;
+    int result;
+
+    for (int i = 4; i <= n; ++i) {
+        result = (a + b + c) % mod;
+        a = b;
+        b = c;
+        c = result;
     }
-    return count;
+
+    return c;
 }
 
 int main() {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
     int n;
-    printf("Введіть натуральне число n: ");
+    printf("Enter the length of the sequence n: ");
     scanf("%d", &n);
 
-    if (n <= 1 || n >= 150) {
-        printf("Число повинно бути в діапазоні 1 < n < 150\n");
+    if (n <= 1 || n >= 10000) {
+        printf("Invalid value of n. It must be 1 < n < 10000.\n");
         return 1;
     }
 
-    int result = equal_divisors(n);
-    printf("Кількість рівних дільників числа %d: %d\n", n, result);
+    int result = countSequences(n);
+    printf("Number of desired sequences: %d\n", result);
 
     return 0;
 }
